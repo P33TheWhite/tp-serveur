aa44deb02f5142f50dc9a56baebd50a44040b91f
Paul Le Blanc <91918397+P33TheWhite@users.noreply.github.com>
2024-04-29 09:28:02 +0200

    Update checkLogin.php

--- login/checkLogin.php
+++ login/checkLogin.php
@@ -1,24 +1,37 @@
 <?php
+session_start();
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST["login"]) && isset($_POST["password"])) {
         $login = $_POST["login"];
         $password = $_POST["password"];
-        // dans info prof
-        $file = fopen("infoTeacher.csv", "r");
-        while(($data=fgetcsv($file)) !== FALSE){  
-            if($data[3]==$login && $data[4]=md5($password)){
-                //go to home.php avec ajouter un cours
-                break;
+        $file = fopen("../Teacher/infoTeacher.csv", "r");
+        $found = false; //utilisateur valide trouvé
+        
+        while (($data = fgetcsv($file)) !== FALSE) {  
+            if ($data[3] == $login && $data[4] == $password) {
+                echo("A");
+                header("Location: home.php");
+                $found = true;
+                exit();
+            } else {
+                if ($login == "admin" && $password == "admin") {
+                    echo("B");
+                    header("Location: home.php?admin=true");
+                    $found = true;
+                    exit();
+                }
             }
         }
-        if($login=="admin" && $password== "admin"){
-            //rediriger vers home.php avec ajouter un prof et ajouter un sport
-        }
-        else{
-            //erreur de login ou de mot de passe renvoyer vers la page login.php
-            header("Location: login.php");
+        
+        fclose($file);
+        
+        if (!$found) {
+            echo("C");
+            $_SESSION["error"] = "Incorrect login or password"; 
+            header("Location: login.php"); // Rediriger vers la page de connexion si erreur de login/password
             exit();
         }
-        fclose($file);
     }
 }
+?>
