8212c5d679057b417aca97c0e57d1bc24409c772
Paul Le Blanc <91918397+P33TheWhite@users.noreply.github.com>
2024-05-11 22:39:23 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
-<?php
-header('Location : menu.php');
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="refresh" content="0; URL=menu.php">
+    <title>Welcome</title>
+</head>
+<body>
+</body>
+</html>
